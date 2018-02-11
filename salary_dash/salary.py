428943f2044c6ececb9ef0b7b97516ddb9e374fa
martinlarsalbert <marale@kth.se>
2018-02-11 17:45:10 +0100

    Added a plot and slider

---
+++ salary_dash/salary.py
@@ -0,0 +1,59 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
+
+app = dash.Dash()
+
+df = pd.read_csv('/Users/martinalexandersson/Dev/lon/2017/pandas/af_data.csv',sep=';')
+years = df['year'].unique()
+
+app.layout = html.Div([
+
+    html.H1(children='Akadmikerföreningens lönestatistik 2018'),
+
+
+    dcc.Graph(
+        id='salary_history',
+        figure={
+            'data': [
+                go.Scatter(
+                    x=df_person['year'],
+                    y=df_person['salary'],
+
+                    text=namn,
+                    mode='lines',
+                    opacity=0.7,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    name=namn
+                ) for namn,df_person in df.groupby('Namn')
+            ],
+
+            'layout': go.Layout(
+                xaxis={'type': 'log', 'title': 'År'},
+                yaxis={'title': 'Lön'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+            )
+        }
+        ),
+
+    dcc.Slider(
+        id='year-slider',
+        min=df['year'].min(),
+        max=df['year'].max(),
+        value=df['year'].max(),
+        step=None,
+        marks={str(year): str(year) for year in years}
+    )
+
+
+])
+
+if __name__ == '__main__':
+    app.run_server()
