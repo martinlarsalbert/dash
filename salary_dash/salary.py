afff44ca69c751f33167190b977bd7186f4044e8
martinlarsalbert <marale@kth.se>
2018-02-11 18:16:00 +0100

    added another plot

--- salary_dash/salary.py
+++ salary_dash/salary.py
@@ -50,10 +50,44 @@ app.layout = html.Div([
         value=df['year'].max(),
         step=None,
         marks={str(year): str(year) for year in years}
-    )
+    ),
+
+    dcc.Graph(id='sallary_year')
 
 
 ])
+
+@app.callback(
+    dash.dependencies.Output('sallary_year', 'figure'),
+    [dash.dependencies.Input('year-slider', 'value')])
+def update_figure(selected_year):
+    year_groups = df.groupby('year')
+    df_year = year_groups.get_group(selected_year)
+
+    line = go.Scatter(
+        x=df_year['Examen'],
+        y=df_year['salary'],
+        text=selected_year,
+        mode='markers',
+        opacity=0.7,
+        marker={
+            'size': 15,
+            'line': {'width': 0.5, 'color': 'white'}
+        },
+        name=selected_year)
+    lines = [line]
+
+    return {
+        'data': lines,
+        'layout': go.Layout(
+            xaxis={'type': 'log', 'title': 'Examensår'},
+            yaxis={'title': 'Lön'},
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+            legend={'x': 0, 'y': 1},
+            hovermode='closest'
+        )
+    }
+
 
 if __name__ == '__main__':
     app.run_server()
